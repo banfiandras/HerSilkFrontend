f7f15cf11f26f1dc7776cdd11e3e04afc76b1441
Aerovity <andras.banfi.hun@gmail.com>
2024-05-13 17:27:06 +0200

    login

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import DetailsView from "../views/DetailsView.vue";
-import Registration from "../views/Registration.vue"
+import Registration from "../views/Registration.vue";
+import Login from "../views/Login.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: "/registration",
       name: "registration",
       component: Registration,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
     }
   ],
 });
---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="col-md-6">
+            <label for="inputEmail4" class="form-label">Username</label>
+            <input type="email" class="form-control" id="inputEmail4">
+        </div>
+        <div class="col-md-6">
+            <label for="inputPassword4" class="form-label">Password</label>
+            <input type="password" class="form-control" id="inputPassword4">
+        </div>
+
+        <div class="col-12">
+            <button type="submit" class="btn btn-primary">Register</button>
+        </div>
+        <div class="col-12">
+            <button type="submit" class="btn btn-primary">Sign in</button>
+        </div>
+</template>
