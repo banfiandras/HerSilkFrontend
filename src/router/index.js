d338ba515686ac913094a22049f1b8328552d5b1
Aerovity <andras.banfi.hun@gmail.com>
2024-05-13 17:16:14 +0200

    rigstration

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import DetailsView from "../views/DetailsView.vue";
+import Registration from "../views/Registration.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       name: "Details",
       component: DetailsView,
     },
+    {
+      path: "/registration",
+      name: "registration",
+      component: Registration,
+    }
   ],
 });
 
---
+++ src/views/registration.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <form class="row g-3">
+        <div class="col-md-6">
+            <label for="inputEmail4" class="form-label">Username</label>
+            <input type="email" class="form-control" id="inputEmail4">
+        </div>
+        <div class="col-md-6">
+            <label for="inputPassword4" class="form-label">Password</label>
+            <input type="password" class="form-control" id="inputPassword4">
+        </div>
+        <div class="col-12">
+            <label for="inputAddress" class="form-label">email:</label>
+            <input type="text" class="form-control" id="inputAddress" placeholder="example@gmail.com">
+        </div>
+        
+        <div class="col-12">
+            <button type="submit" class="btn btn-primary">Sign in</button>
+        </div>
+    </form>
+
+</template>
